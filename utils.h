aa9b7a8b61586c3e0a7d46fc4e984e71d8127d08
mzdevI <muhammed.mammadhuseynov@gmail.com>
2023-04-14 18:43:52 +0400

    commit

---
+++ utils.h
@@ -0,0 +1,7 @@
+#ifndef UNTITLED_UTILS_H
+#define UNTITLED_UTILS_H
+
+float max(const int arr[], int arrSize);
+float min(const int arr[], int arrSize);
+float avg(const int arr[], int arrSize);
+#endif
