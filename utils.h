8265a62a2b4f6c0aaabd149364dfeea927f68bcf
mz <118102747+mzdevI@users.noreply.github.com>
2023-04-14 18:53:50 +0400

    Delete utils.h

--- utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UNTITLED_UTILS_H
-#define UNTITLED_UTILS_H
-
-float max(const int arr[], int arrSize);
-float min(const int arr[], int arrSize);
-float avg(const int arr[], int arrSize);
-#endif
