7f3247a2eacd55b3ca7386d2e4b9a8b7ee844f53
muhammed <muhammed.mammadhuseynov@gmail.com>
2023-10-29 19:06:11 +0400

    add project

---
+++ Auth-24-10-2023/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using Auth_24_10_2023.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth_24_10_2023.Models;
+
+public class User
+{
+    public string Email { get; }
+
+    public string Password { get; private set; }
+
+    public User(string email, string password)
+    {
+        if (EmailValidation.ValidateEmail(email) == true)
+        {
+            Email = email;
+        }
+        else
+        {
+            throw new ArgumentException($"{email} is not a valid email");
+        }
+
+        Password = password;
+    }
+
+    public void ResetPassword(string newPassword)
+    {
+        Password = newPassword;
+    }
+}
