6bb07a9240fa1c290882e11cb2206f17975af307
mz-mm <muhammed.mammadhuseynov@gmail.com>
2023-12-20 11:55:28 +0400

    Add NavigationWindow

---
+++ NavigationWindow/NavigationWindow/Pages/InfoView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace NavigationWindow.Pages;
+
+public partial class InfoView : Page
+{
+    public InfoView(Forecast forecast)
+    {
+        InitializeComponent();
+
+        tempLbl.Content = forecast.Main.Temp;
+        pressureLbl.Content = forecast.Main.Pressure;
+        feelsLikeLbl.Content = forecast.Main.FeelsLike;
+
+        weatherImg.Source = new BitmapImage(new Uri($"http://openweathermap.org/img/w/{forecast.Weather[0].Icon}.png"));
+
+        sunsetLbl.Content = GetDateByUnixSeconds(forecast.Sys.Sunset).AddSeconds(forecast.Timezone).ToString("HH:mm:ss");
+        sunriseLbl.Content = GetDateByUnixSeconds(forecast.Sys.Sunrise).AddSeconds(forecast.Timezone).ToString("HH:mm:ss");
+
+
+    }
+    private DateTime GetDateByUnixSeconds(int seconds)
+    {
+        DateTime date = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddSeconds(seconds);
+        return date;
+    }
+}
---
+++ NavigationWindow/NavigationWindow/Pages/SearchView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+namespace NavigationWindow.Pages;
+
+public partial class SearchView : Page
+{
+    public List<Forecast> SearchResults { get; set; }
+
+    public SearchView()
+    {
+        InitializeComponent();
+
+        SearchResults = GetAllSearch() ?? new();
+        searchResults.ItemsSource = SearchResults;
+    }
+
+    private List<Forecast>? GetAllSearch()
+    {
+        using FileStream fs = new("results.json", FileMode.OpenOrCreate);
+        using StreamReader sr = new(fs);
+
+        string? json = sr.ReadToEnd();
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<List<Forecast>>(json);
+    }
+
+    private void SaveAllSearch()
+    {
+        using FileStream fs = new("results.json", FileMode.OpenOrCreate);
+        using StreamWriter sw = new(fs);
+
+        string json = JsonSerializer.Serialize(SearchResults);
+
+        sw.Write(json);
+    }
+
+    private void searchButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var res = WeatherService.GetWeatherByCity(searchBox.Text);
+
+            res.SearchDate = DateTime.Now;
+            res.name = searchBox.Text.ToLower();
+
+            SearchResults.Add(res);
+
+            searchResults.Items.Refresh();
+
+            SaveAllSearch();
+
+            searchBox.Clear();
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+    private void searchResults_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        this.NavigationService.Navigate(new InfoView(searchResults.SelectedItem as Forecast));
+    }
+}
