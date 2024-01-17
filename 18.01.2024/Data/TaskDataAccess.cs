dca0312ef9c08cd8394897a356da5803ec6d48ba
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-01-17 21:31:38 +0400

    Add hw

---
+++ 18.01.2024/Data/TaskDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+
+namespace _18._01._2024.Data;
+
+public class TaskDataAccess
+{
+    public DataTable CreateTaskTable()
+    {
+        DataTable dataTable = new DataTable("Tasks");
+
+        dataTable.Columns.Add("Id", typeof(int));
+        dataTable.Columns.Add("Name", typeof(string));
+
+        return dataTable;
+    }
+}
---
+++ 18.01.2024/Repository/ITaskRepository.cs
@@ -0,0 +1,12 @@
+﻿using _18._01._2024.Models;
+using System.Collections.Generic;
+
+namespace _18._01._2024.Repository;
+
+public interface ITaskRepository
+{
+    public List<Task> GetAllTask();
+    public Task CreateTask(Task task);
+    public Task UpdateTask(Task task);
+    public bool DeleteTask(int taskId);
+}
